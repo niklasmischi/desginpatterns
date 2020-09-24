749c39dd43ab3266a965e1e8906bd2a1b788e268
Niklas Mischi <niklas.mischi@gmail.com>
2020-09-24 14:40:11 +0200

    added interface to MainGame

--- Games/src/at/mis/games/wintergame/MainGame.java
+++ Games/src/at/mis/games/wintergame/MainGame.java
@@ -1,16 +1,18 @@
 package at.mis.games.wintergame;
 
+import at.mis.games.wintergame.actors.Actor;
 import org.newdawn.slick.*;
 import at.mis.games.wintergame.actors.HTLCircle;
 import at.mis.games.wintergame.actors.HTLOval;
 import at.mis.games.wintergame.actors.HTLRectangle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainGame extends BasicGame {
 
-    public HTLRectangle rect1;
-    public HTLOval oval1;
-    public HTLCircle circle1;
+    private List<Actor> actors;
 
     public MainGame(String title) {
         super(title);
@@ -18,23 +20,25 @@ public class MainGame extends BasicGame {
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-        this.rect1 = new HTLRectangle(10,10,100,100);
-        this.oval1 = new HTLOval(10,10,30,30);
-        this.circle1 = new HTLCircle(10,10, 20,20);
+        this.actors = new ArrayList<>();
+
+        this.actors.add(new HTLRectangle(10,10,100,100));
+        this.actors.add(new HTLOval(10,10,30,30));
+        this.actors.add(new HTLCircle(10,10, 20,20));
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
-        rect1.update(gameContainer, delta);
-        oval1.update(gameContainer, delta);
-        circle1.update(gameContainer, delta);
+        for (Actor actor : this.actors) {
+            actor.update(gameContainer, delta);
+        }
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        rect1.render(graphics);
-        oval1.render(graphics);
-        circle1.render(graphics);
+        for (Actor actor : this.actors) {
+            actor.render(graphics);
+        }
     }
 
     public static void main(String[] argv) {
